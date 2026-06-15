1359e8f40e718f2d65ce0e99df8dc49651991272
Dhille111 <dhilleraokotilingala@gmail.com>
2026-06-16 01:08:30 +0530

    switch models to standard h5 format to bypass keras zip deserialization bug

--- utils/config.py
+++ utils/config.py
@@ -13,8 +13,8 @@ RIGHT_EYE_LANDMARKS = list(range(42, 48))
 # ==========================================
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-MODEL_CNN_PATH = os.path.join(BASE_DIR, "saved_models", "cnn_model.keras")
-MODEL_BILSTM_PATH = os.path.join(BASE_DIR, "saved_models", "bilstm_model.keras")
+MODEL_CNN_PATH = os.path.join(BASE_DIR, "saved_models", "cnn_model.h5")
+MODEL_BILSTM_PATH = os.path.join(BASE_DIR, "saved_models", "bilstm_model.h5")
 SHAPE_PREDICTOR_PATH = os.path.join(BASE_DIR, "models", "shape_predictor_68_face_landmarks.dat", "shape_predictor_68_face_landmarks.dat")
 
 DATASET_DIR = os.path.join(BASE_DIR, "dataset", "eye_sequences")
